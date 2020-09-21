911c94fc85315a6ad1d7abdf8bbaac20ed533380
Artem Chesnokov <artem@chesnokov.pp.ua>
2020-09-21 17:25:53 +0300

    Refactoring

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -1,60 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import s from './Paginator.module.css'
 
-class Paginator extends Component {
-    state = {
-        currentPortion: 1
+const Paginator = props => {
+    let [currentPortion, setCurrentPortion] = useState(1)
+    const pagesCount = Math.ceil(props.totalItemsCount / props.pageSize)
+    const portionSize = props.portionSize ? props.portionSize : 10
+    const portionsCount = Math.ceil(props.totalItemsCount / portionSize)
+    const portionFirst = (currentPortion - 1) * portionSize + 1
+    let portionLast = currentPortion * portionSize
+    portionLast = portionLast > pagesCount ? pagesCount : portionLast
+    let pages = []
+    for (let i = portionFirst; i <= portionLast; i++) {
+        pages.push(i)
     }
 
-    render () {
-        const pagesCount = Math.ceil(this.props.totalItemsCount / this.props.pageSize)
-        const portionSize = this.props.portionSize ? this.props.portionSize : 10
-        const portionsCount = Math.ceil(this.props.totalItemsCount / portionSize)
-        const portionFirst = (this.state.currentPortion - 1) * portionSize + 1
-        let portionLast = this.state.currentPortion * portionSize
-        portionLast = portionLast > pagesCount ? pagesCount : portionLast
-        let pages = []
-        for (let i = portionFirst; i <= portionLast; i++) {
-            pages.push(i)
-        }
-
-        return (
-            <div className={s.paginatorContainer}>
-                {this.state.currentPortion > 1 &&
-                    <button
-                        key='prev'
-                        className={s.prevButton}
-                        onClick={() => {
-                            this.setState(prevState => ({currentPortion: prevState.currentPortion - 1}))
-                        }}
-                    >
-                        prev
+    return (
+        <div className={s.paginatorContainer}>
+            {currentPortion > 1 &&
+                <button
+                    key='prev'
+                    className={s.prevButton}
+                    onClick={() => {
+                        setCurrentPortion(currentPortion - 1)
+                    }}
+                >
+                    prev
                     </button>
-                }
-                {pages.map(page =>
-                    <button
-                        key={page}
-                        className={s.pageButton + ' ' + (page === this.props.currentPage ? s.selectedPage : '')}
-                        onClick={() => {
-                            this.props.onPageChanged(page)
-                        }}
-                    >
-                        {page}
+            }
+            {pages.map(page =>
+                <button
+                    key={page}
+                    className={s.pageButton + ' ' + (page === props.currentPage ? s.selectedPage : '')}
+                    onClick={() => {
+                        props.onPageChanged(page)
+                    }}
+                >
+                    {page}
+                </button>
+            )}
+            {portionsCount > currentPortion &&
+                <button
+                    key='next'
+                    onClick={() => {
+                        setCurrentPortion(currentPortion + 1)
+                    }}
+                >
+                    next
                     </button>
-                )}
-                {portionsCount > this.state.currentPortion &&
-                    <button
-                        key='next'
-                        onClick={() => {
-                            this.setState(prevState => ({currentPortion: prevState.currentPortion + 1}))
-                        }}
-                    >
-                        next
-                    </button>
-                }
-            </div>
-        )
-    }
+            }
+        </div>
+    )
 }
 
 export default Paginator
