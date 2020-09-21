c2545fabf15c55163eee4b38983123aeaf728f51
Artem Chesnokov <artem@chesnokov.pp.ua>
2020-09-21 17:26:10 +0300

    Tests

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,12 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
-import dialogsReducer from "./dialogs-reducer"
-import profileReducer from "./profile-reducer"
-import navbarReducer from "./navbar-reducer"
-import usersReducer from "./users-reducer"
-import authReducer from "./auth-reducer"
-import reduxThunk from "redux-thunk"
+import { applyMiddleware, combineReducers, compose, createStore } from "redux"
 import { reducer as formReducer } from "redux-form"
+import reduxThunk from "redux-thunk"
 import appReducer from "./app-reducer"
+import authReducer from "./auth-reducer"
+import dialogsReducer from "./dialogs-reducer"
+import navbarReducer from "./navbar-reducer"
+import profileReducer from "./profile-reducer"
+import usersReducer from "./users-reducer"
 
 let reducers = combineReducers({
     profileReducer,
@@ -18,6 +18,8 @@ let reducers = combineReducers({
     form: formReducer
 })
 
-let store = createStore(reducers, applyMiddleware(reduxThunk))
+// let store = createStore(reducers, applyMiddleware(reduxThunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))
 
 export default store
